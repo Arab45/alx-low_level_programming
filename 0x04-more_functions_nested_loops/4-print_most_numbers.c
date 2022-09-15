2921baebd461a883df8a93cf3a3d869ca3a48f17
Arab45 <Arab45>
2022-09-15 16:24:56 -0700

    Printed Number 1

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*print_most_numbers - prints
+* owned by CNN
+*Return: void
+*/
+void print_most_numbers(void)
+{
+	char y;
+
+	for (y = '0'; y <='9'; y++)
+	{
+	if (!(y == '2' || y == '4'))
+	_putchar(y);
+	}
+	_putchar('\n');
+}	
+
