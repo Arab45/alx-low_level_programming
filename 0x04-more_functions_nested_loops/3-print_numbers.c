e010c2b5978a4579620b862379a65a84eec423b0
Arab45 <Arab45>
2022-09-15 16:02:48 -0700

    Printed Number

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*print_numbers - print 0123456789
+* owned by CNN
+*Return: void
+*/
+void print_numbers(void)
+{
+	char y;
+
+	for (y = '0'; y <= '9'; y++)
+	{
+	_putchar(y);
+	}
+	_putchar('\n');
+}
